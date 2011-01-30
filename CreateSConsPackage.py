5a1c8367cace7cc6b9461734bda3afef9754219d
Richard Viney <richard.viney@gmail.com>
2011-01-31 01:32:54 +1300

    Initial import

---
+++ CreateSConsPackage.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#
+# This script creates a Mac OS X .pkg installer for SCons from a SCons tarball passed on the command line.
+#
+#       e.g. python CreateSConsPackage.py scons-2.0.1.tar.gz
+#   
+
+import atexit, os, sys, tempfile
+
+if len(sys.argv) != 2:
+    print "A SCons tarball must be passed as a command line parameter"
+    exit(1)
+
+sconsTarball = os.path.abspath(sys.argv[1])
+if not os.path.exists(sconsTarball):
+    print "The specified SCons tarball does not exist: %s" % sconsTarball
+    exit(1)
+
+# Read the version string from the tarball name
+sconsVersion = os.path.basename(sconsTarball)[6:-7]
+packageName = "SCons-%s" % sconsVersion
+
+print "Creating Mac OS X package for SCons %s from tarball '%s' ..." % (sconsVersion, sconsTarball)
+
+# This is where the contents of the SCons tarball are put on the target machine. The normal 'python setup.py install'
+# process is then run from this location and the directory is subsequently deleted by the PostInstall script.
+targetTempDirectory = "/tmp/SConsInstaller"
+
+print "Creating package directory structure ..."
+packageDirectory = "%s/SConsPackage" % tempfile.gettempdir()
+installedDirectory = packageDirectory + targetTempDirectory
+os.system("rm -rf %s && mkdir -p %s" % (installedDirectory, installedDirectory))
+
+# Clean up package directory on exit
+def cleanupPackageDirectory():
+    os.system("rm -rf %s" % packageDirectory)
+atexit.register(cleanupPackageDirectory)
+
+print "Extracting and relocating SCons tarball ..."
+if os.system("tar -C %s -xf %s && mv %s/scons-%s/* %s" % (installedDirectory, sconsTarball, installedDirectory, sconsVersion, installedDirectory)) != 0:
+    print "*** Failed extracting and relocating SCons tarball ***"
+    exit(1)
+
+print "Creating package with PackageMaker ..."
+if os.system("/Developer/usr/bin/packagemaker \
+    --title \"SCons %s\" \
+    --id com.tigris.SCons.pkg \
+    --target 10.5 \
+    --install-to / \
+    --root-volume-only \
+    --root %s \
+    --filter \"\.DS_Store\" \
+    --filter \"\.svn\" \
+    --out \"%s.pkg\" \
+    --scripts Scripts/ \
+    --no-relocate" \
+    % (sconsVersion, packageDirectory, packageName)) != 0:
+    print "*** Failed creating package ***"
+    exit(1)
+
+print "Success, '%s.pkg' was created" % packageName
---
+++ README.md
@@ -0,0 +1,11 @@
+SCons Mac OS X Installer
+========================
+
+Takes a [SCons](http://www.scons.org/) release tarball and makes a standard Mac OS X GUI installer for it, which can then be used to install SCons on Mac OS X instead of needing to install via the command line.
+
+Mac OS X installer downloads for recent SCons releases are provided:
+
+* [SCons 2.0.1 installer for Mac OS X](https://github.com/downloads/rviney/scons-mac-installer/SCons-2.0.1.pkg)
+* [SCons 1.3.1 installer for Mac OS X](https://github.com/downloads/rviney/scons-mac-installer/SCons-1.3.1.pkg)
+
+Note: There is a SCons [enhancement request](http://scons.tigris.org/issues/show_bug.cgi?id=2681) for making a Mac OS X installer a standard part of a SCons release, which would render the scripts in this repository obsolete.
