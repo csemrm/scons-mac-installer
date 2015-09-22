6eb4a76081f9419d2af927f07f571c859694b1a1
Richard Viney <richard.viney@gmail.com>
2015-09-22 17:20:38 +1200

    Python 3 compatibility

--- create_scons_package.py
+++ create_scons_package.py
@@ -11,12 +11,12 @@ import sys
 import tempfile
 
 if len(sys.argv) != 2:
-    print 'Error: A SCons tarball must be passed as a command line argument'
+    print('Error: A SCons tarball must be passed as a command line argument')
     exit(1)
 
 tarball = os.path.abspath(sys.argv[1])
 if not os.path.exists(tarball):
-    print 'Error: The specified SCons tarball does not exist: %s' % tarball
+    print('Error: The specified SCons tarball does not exist: %s' % tarball)
     exit(1)
 
 # Read the version string from the tarball name
@@ -33,17 +33,17 @@ os.system('mkdir -p %s' % target_directory)
 # Extract tarball
 if os.system('tar -C %s --strip-components 1 -xzf %s' %
              (target_directory, tarball)) != 0:
-    print 'Error: Failed extracting SCons tarball'
+    print('Error: Failed extracting SCons tarball')
     exit(1)
 
 # Build the package
 if os.system('xcrun pkgbuild --quiet --identifier com.scons.SCons.pkg \
               --install-location / --root %s --scripts scripts/ "%s.pkg"' %
              (package_directory, package_name)) != 0:
-    print 'Error: Failed creating package'
+    print('Error: Failed creating package')
     exit(1)
 
 # Cleanup
 os.system('rm -rf %s' % package_directory)
 
-print 'Success, %s.pkg was created' % package_name
+print('Success, %s.pkg was created' % package_name)
